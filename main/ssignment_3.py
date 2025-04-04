704d07a4c36ffcd9cb95cefa4a4eb24c49aa9ea4
IvanS <125488677+IvanSchndr@users.noreply.github.com>
2025-04-04 19:22:03 -0400

    Update ssignment_3.py

--- main/ssignment_3.py
+++ main/ssignment_3.py
@@ -26,7 +26,90 @@ def euler(f, tn, yn, h):
 
 def f(t,y):
   return t-y*y
+
+
+def gauss(a, b):
+  n = len(b)
+  m = n-1
+  i = 0
+  j = i-1
+  x = np.zeros(n)
+
+  new = np.concatenate((a, b), axis=1, dtype=float)
+
+
+  while (i < n):
+    if (new[i][i] != 0):
+      for j in range(i + 1, n):
+        scaling =( new[j][i] )/ (new[i][i])
+        new[j] = new[j] - (scaling * new[i])
+   
+    i = i + 1
   
+  x[m] = new[m][n] / new[m][m]
+
+  for k in range(n - 2, -1, -1):
+    x[k] = new[k][n]
+
+    for j in range(k + 1, n):
+      x[k] = x[k] - new[k][j] * x[j]
+
+    x[k] = x[k] / new[k][k]
+  print("\n[", end = " ")
+  for ans in range(n):
+    z = x[ans]
+    print(f"{z:.2f}", end=" ")
+  print("]", )
+
+def l_u(a):
+  n = len(a)
+  i = 0
+  j = i-1
+  
+
+  U = np.zeros((n,n))
+  U = a.astype(float)
+  L = np.zeros((n,n))
+  L = L.astype(float)
+
+  while (i < n):
+    if (U[i][i] != 0):
+      for j in range(i + 1, n):
+        scaling =( U[j][i] )/ (U[i][i])
+        L[j][i] = scaling
+        U[j] = U[j] - (scaling * U[i])
+
+    i = i + 1
+  for k in range(n):
+    L[k][k] = 1
+
+  d = np.linalg.det(L) * np.linalg.det(U)
+    
+  print("\n", d, "\n\n", L, "\n\n", U)
+
+
+def isdidominant(a):
+  for i in range(len(a)):
+    sum = 0
+    for j in range(len(a)):
+      if (i != j):
+        sum += abs(a[i][j])
+    if (abs(a[i][i]) <= sum):
+      return False
+
+  return True
+
+
+def isposdef(a):
+  for i in range(len(a)):
+    for j in range(i, len(a)):
+      if (a[i][j] != a[j][i]):
+        return False
+  ig = np.linalg.eigvals(a)
+  for i in range(len(ig)):
+    if (ig[i] < 0):
+      return False
+  return True
 
 def main():
   r = [0, 2]
@@ -35,9 +118,32 @@ def main():
   f0 = 1
   y = IVP(f, r, f0, h, euler)
   print(y[10])
+  print("\n")
   y = IVP(f, r, f0, h, rk)
   print(y[10])
-  
+  print("")
+
+  a = np.array([[2, -1, 1], [1, 3, 1], [-1, 5, 4]])
+  at = np.array([[1, 1, 0, 3], [2, 1, -1, 1], [3, -1, -1, 2], [-1, 2, 3, -1]])
+  b = np.array([[6], [0], [-3]])
+ 
+  gauss(a, b)
+  print("")
+
+  l_u(at)
+
+  dd = np.array([[9, 0, 5, 2, 1], [3, 9, 1, 2, 1], [0, 1, 7, 2, 3],[4, 2, 3, 12, 2],[3, 2, 4, 0, 8]])
+  if isdidominant(dd):
+    isdd = "YES"
+  else:
+    isdd = "NO"
+  print("\n\n3. Is the matrix diagonally dominant:", isdd)
+  pd = np.array([[2,2,1],[2,3,0],[1,0,2]])
+  if isposdef(pd):
+    ispd = "YES"
+  else:
+    ispd = "NO"
+  print("\n4. Is the matrix a positive definite:", ispd)
 
 if __name__ == "__main__":
   main()
